cdae1913d231ff3f7d73343d4d4584695e5c1ce9
George <georgehm@pm.me>
2021-07-07 08:44:56 +0100

    Validate request and return ping

--- src/command_handler/handler.js
+++ src/command_handler/handler.js
@@ -1,5 +1,37 @@
-const knex = require('knex');
+const db = require('../database.js');
+const constants = require('../constants.js');
 
-module.exports.handler = (event, context) => {
+module.exports.handler = event => {
+    if (!constants.validateRequest(event)) {
+        return constants.createResponse(401);
+    }
 
+    if (!event.body) {
+        throw new Error('Missing body, should not happen');
+    }
+
+    const body = JSON.parse(event.body);
+
+    // discord is pinging us, pong
+    if (body.type === 1) {
+        return constants.createResponse(
+            200,
+            {
+                type: 1,
+            },
+        );
+    }
+
+    const commandData = body.data;
+    if (!commandData) {
+        return constants.createResponse(
+            200,
+            // message saying cant find command
+        );
+    }
+
+    const commandName = commandData.name;
+    // pass to mapping of command name -> command model
+
+    db.close();
 };
