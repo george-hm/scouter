c06892f357d381738554c996c18d1ad0155f91ad
George <georgehm@pm.me>
2021-07-10 17:58:00 +0100

    Start command verify data

--- src/model/Command.js
+++ src/model/Command.js
@@ -1,4 +1,24 @@
+const User = require('./User.js');
+
 // https://discord.com/developers/docs/interactions/slash-commands#interaction-object-application-command-interaction-data-structure
-class Command { }
+class Command {
+    constructor(commandData, user) {
+        if (!(user instanceof User)) {
+            throw new Error('User is not model');
+        }
+        this._id = commandData.id;
+        this._name = commandData.name;
+        this._options = commandData.options;
+        this._user = user;
+    }
+
+    async main() {
+        throw new Error('Main function not implemented');
+    }
+
+    getCommandName() {
+        return this._name;
+    }
+}
 
 module.exports = Command;
