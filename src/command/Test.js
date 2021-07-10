8b92dfb8a30b4567fc2117bdb02f520fb798c173
George <georgehm@pm.me>
2021-07-10 19:18:22 +0100

    Rename commands to command

--- src/command/Test.js
+++ src/command/Test.js
@@ -1,7 +1,10 @@
 const Command = require('./Command.js');
+const InteractionResponse = require('../model/InteractionResponse.js');
 
 class Test extends Command {
+    async main() {
 
+    }
 }
 
 module.exports = Test;
--- src/model/DiscordEvent.js
+++ src/model/DiscordEvent.js
@@ -1,5 +1,5 @@
-const allCommands = require('../commands/index.js');
-const Command = require('../commands/Command.js');
+const allCommands = require('../command/index.js');
+const Command = require('../command/Command.js');
 const User = require('./User.js');
 
 // https://discord.com/developers/docs/interactions/slash-commands#interaction-object
