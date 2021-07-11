535692b360807d04169e85a019ff72753a11ee35
George <georgehm@pm.me>
2021-07-11 18:21:01 +0100

    Support custom id and set as name

--- src/command/Command.js
+++ src/command/Command.js
@@ -10,6 +10,10 @@ class Command {
         this._name = commandData.name;
         this._options = commandData.options;
         this._user = user;
+        this._customId = commandData.custom_id;
+        if (!this._name && this._customId) {
+            this._setCommandNameFromCustomId();
+        }
     }
 
     async main() {
@@ -18,6 +22,25 @@ class Command {
 
     getCommandName() {
         return this._name;
+    }
+
+    _setCommandNameFromCustomId() {
+        const parts = this._customId.split('.');
+        if (!parts[0]) {
+            throw new Error('Invalid custom id');
+        }
+        this._name = parts[0];
+    }
+
+    static createCustomId(name, user) {
+        return `${name}.${user.getUserId()}`;
+    }
+
+    validateCustomIdBelongsToUser() {
+        const parts = this._customId.split('.');
+        const userId = parts[1];
+
+        return userId === this._user.getUserId();
     }
 }
 
