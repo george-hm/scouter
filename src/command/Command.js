a9da403974acb72a2a5ce27add3259438c632f86
George <georgehm@pm.me>
2021-07-12 08:36:08 +0100

    Handle custom ids and use that as name

--- src/command/Command.js
+++ src/command/Command.js
@@ -7,33 +7,42 @@ class Command {
             throw new Error('User is not model');
         }
         this._id = commandData.id;
-        this._name = commandData.name;
+        this._name = Command.getCommandName(commandData);
         this._options = commandData.options;
         this._user = user;
         this._customId = commandData.custom_id;
-        if (!this._name && this._customId) {
-            this._setCommandNameFromCustomId();
-        }
     }
 
     async main() {
         throw new Error('Main function not implemented');
     }
 
+    getUser() {
+        return this._user;
+    }
+
     getCommandName() {
         return this._name;
     }
 
-    _setCommandNameFromCustomId() {
-        const parts = this._customId.split('.');
+    static getCommandName(data) {
+        if (data.name) {
+            return data.name;
+        }
+
+        if (!data.custom_id) {
+            throw new Error('No name or custom id');
+        }
+
+        const parts = data.custom_id.split('.');
         if (!parts[0]) {
             throw new Error('Invalid custom id');
         }
-        this._name = parts[0];
+        return parts[0];
     }
 
-    static createCustomId(name, user) {
-        return `${name}.${user.getUserId()}`;
+    createCustomId(name) {
+        return `${name}.${this.getUser().getUserId()}`;
     }
 
     validateCustomIdBelongsToUser() {
