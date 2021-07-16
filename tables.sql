36e7918f306f05f7686778ecf2a1d5e857143c1b
George <georgehm@pm.me>
2021-07-16 04:25:38 +0100

    Create DJ for tables

---
+++ tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `character` (
+  `id` bigint NOT NULL,
+  `resource_id` bigint NOT NULL,
+  `cost` bigint NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `secondname` varchar(512) NOT NULL,
+  `avg_stats` bigint NOT NULL,
+  `v_rarity` tinyint NOT NULL,
+  `rarity` varchar(20) NOT NULL,
+  `class` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `leader` TEXT NOT NULL,
+  `super` JSON NOT NULL,
+  `passive` JSON NOT NULL,
+  `links` JSON NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `v_rarity` (`v_rarity`)
+);
+
+CREATE TABLE `category` (
+  `category` varchar(128) NOT NULL,
+  `id` bigint NOT NULL,
+  PRIMARY KEY (`category`, `id`),
+  KEY `category` (`category`)
+);
