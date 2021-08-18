3dd19141e053686a2259e4cfe6dfe87a9a4049b4
George <georgehm@pm.me>
2021-08-18 08:49:59 +0100

    Update for granting rewards

--- src/command/HourlyCheckIn.js
+++ src/command/HourlyCheckIn.js
@@ -1,23 +1,28 @@
 const Command = require('./Command.js');
 const InteractionResponse = require('../model/discord/InteractionResponse.js');
-const Component = require('../model/discord/Component.js');
-const time = require('../time.js');
+const Constants = require('../constants.js');
+const Time = require('../time.js');
 
 class HourlyCheckIn extends Command {
     async main() {
         const user = this.getUser();
         await user.loadPlayerInfo();
         const lastHourlyCheckIn = user.lastHourlyCheckIn;
-        if (!time.eligableForHourly(lastHourlyCheckIn)) {
-            const timeUntilHourly = time.timeUntilHourly(lastHourlyCheckIn);
+        if (!Time.eligableForHourly(lastHourlyCheckIn)) {
+            const timeUntilHourly = Time.timeUntilHourly(lastHourlyCheckIn);
             return new InteractionResponse(
                 InteractionResponse.RESPOND,
                 `Sorry, come back in ${timeUntilHourly} for your hourly check-in`,
             );
         }
+
+        const rewardValue = user.grantHourlyReward();
+        user.lastHourlyCheckIn = Constants.getTime();
+        await user.save();
+
         return new InteractionResponse(
             InteractionResponse.RESPOND,
-            'Not implemented',
+            `<:bardockdisgust:850378401743110164> I see you've found ${rewardValue} Z-Orbs.\n**Streak**: ${user.hourlyStreak}`,
         );
     }
 }
