78854c218545a8e462f611556bb961c6927db86e
George <georgehm@pm.me>
2021-09-08 17:44:26 +0100

    Be more specific for constructor

--- src/command/Command.js
+++ src/command/Command.js
@@ -2,15 +2,14 @@ const User = require('../model/discord/User.js');
 
 // https://discord.com/developers/docs/interactions/slash-commands#interaction-object-application-command-interaction-data-structure
 class Command {
-    constructor(commandData, user) {
+    constructor(commandName, customId, options, user) {
         if (!(user instanceof User)) {
             throw new Error('User is not model');
         }
-        this._id = commandData.id;
-        this._name = commandData.commandName;
-        this._options = commandData.options;
+        this._commandName = commandName;
+        this._options = options;
         this._user = user;
-        this._customId = commandData.custom_id;
+        this._customId = customId;
     }
 
     async main() {
@@ -19,10 +18,6 @@ class Command {
 
     getUser() {
         return this._user;
-    }
-
-    getCommandName() {
-        return this._name;
     }
 
     static toJSON() {
@@ -34,10 +29,18 @@ class Command {
     }
 
     validateCustomIdBelongsToUser() {
+        if (!this._customId) {
+            return false;
+        }
+
         const parts = this._customId.split('.');
         const userId = parts[1];
 
         return userId === this._user.getUserId();
+    }
+
+    get commandName() {
+        return this._commandName;
     }
 
     static get commandName() {
