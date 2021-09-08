80032fd80620ccc7c71f2a65fbcf793eba8042e5
George <georgehm@pm.me>
2021-09-08 17:45:19 +0100

    Check for button and create interaction instance

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,13 @@
 const { Client, Intents } = require('discord.js');
 const Database = require('./database.js');
-const DiscordEvent = require('./model/discord/DiscordEvent.js');
+const Interaction = require('./model/discord/Interaction.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+    if (!interaction.isCommand() && !interaction.isButton()) return;
 
-    const event = new DiscordEvent(interaction);
+    const event = new Interaction(interaction);
     console.log(event.getLogMessage());
     const command = event.getCommand();
     const response = await command.main();
