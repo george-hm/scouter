ee25b75f4141baaeee5c1603dfe82148ad2bd9d1
George <georgehm@pm.me>
2021-09-22 06:51:41 +0100

    0 not null

--- src/model/Banner.js
+++ src/model/Banner.js
@@ -85,7 +85,7 @@ class Banner {
         const bannersToUpdate = await db(tableBanner)
             .select(keyBannerId)
             .where({
-                [keyExpires]: null,
+                [keyExpires]: 0,
             })
             .orderByRaw('RAND()');
 
@@ -97,7 +97,7 @@ class Banner {
             }
             await db(tableBanner)
                 .update({
-                    [keyExpires]: null,
+                    [keyExpires]: 0,
                 });
 
             return await this._addNewBanners(true);
