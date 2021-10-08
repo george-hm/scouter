12f1176c2cb20056777505d53f6b0017f32abbfb
George <24510197+george-hm@users.noreply.github.com>
2021-10-08 05:02:22 +0100

    Fix if

--- src/time.js
+++ src/time.js
@@ -156,7 +156,7 @@ class Time {
 
     // 1 = monday 7 = sunday
     static getUpcomingDayOfWeek(dayOfWeek) {
-        if ((!dayOfWeek && dayOfWeek !== 0) || dayOfWeek > 6) {
+        if (!dayOfWeek || dayOfWeek > 7) {
             throw new Error(`Invalid day of week: ${dayOfWeek}`);
         }
 
