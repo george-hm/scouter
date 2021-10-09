b7166163ed0c760ac74c818fdde26364f62f0686
George <georgehm@pm.me>
2021-10-09 16:24:17 +0100

    Add multi summon

--- src/command/Summon.js
+++ src/command/Summon.js
@@ -4,6 +4,7 @@ const InteractionResponse = require('../model/discord/InteractionResponse.js');
 const Component = require('../model/discord/Component.js');
 const Character = require('../model/Character.js');
 const Banner = require('../model/Banner.js');
+const Embed = require('../model/discord/Embed.js');
 
 const customIdEmpty = 'empty';
 
@@ -20,10 +21,10 @@ class Summon extends Command {
                 `${user.getMention()} <:babysmirk:850200356495687680> you need ${Summon.summonCost} ${Character.getZOrbEmoji()} Z-Orbs to do that`,
             );
         }
-        const roll = Character.getRandomRarity();
         const bannerId = this.getBannerId();
-        let summonedCharacter = null;
         let bannerMessage = '';
+
+        let summoner = null;
         if (bannerId) {
             const loadedBanner = await Banner.getBannerById(bannerId);
             if (!loadedBanner) {
@@ -31,13 +32,23 @@ class Summon extends Command {
             }
 
             bannerMessage = `from banner **${loadedBanner.getBannerName()}**`;
-            summonedCharacter = await loadedBanner.summon(roll);
+            summoner = loadedBanner.summon;
         } else {
-            summonedCharacter = await Character.getRandomByRarity(roll);
+            summoner = Character.getRandomByRarity;
         }
-        summonedCharacter.addToPlayer(user.getUserId());
-        user.addRarityToInventory(roll);
-        user.currency -= Summon.summonCost;
+
+        // TODO: change this and dont wait in loop
+        // This is HORRIBLE and does more db calls then it should but hey its easy
+        const allSummons = [];
+        let roll;
+        for (let i = 0; i < this.getSummonCount(); i++) {
+            roll = Character.getRandomRarity();
+            const summonedCharacter = await summoner(roll);
+            allSummons.push(summonedCharacter);
+            await summonedCharacter.addToPlayer(user.getUserId());
+            user.addRarityToInventory(roll);
+            user.currency -= Summon.summonCost;
+        }
 
         await user.save();
 
@@ -53,9 +64,20 @@ class Summon extends Command {
                 this.createCustomId(bannerId),
             ),
         ]);
+
+        let embedToReturn = new Embed(
+            `${user.getMention()} rolled ${this.getSummonCount()} characters ${bannerMessage}`,
+            allSummons.map(character => character.stringSummary),
+        );
+        if (!allSummons.length) {
+            throw new Error('No summons');
+        }
+        if (allSummons.length === 1) {
+            embedToReturn = allSummons[0].toEmbed();
+        }
         return new InteractionResponse(
             `${user.getMention()} you rolled: ${Character.convertRarityToEmoji(roll)} ${bannerMessage}`,
-            [summonedCharacter.toEmbed()],
+            [embedToReturn],
             component,
         );
     }
@@ -70,21 +92,41 @@ class Summon extends Command {
         return parseInt(customIdParts[1]) || null;
     }
 
-    createCustomId(bannerId) {
-        return Summon.createCustomId(bannerId, this.getUser().getUserId());
+    getSummonCount() {
+        const customId = this._customId;
+        if (!customId) {
+            return 1;
+        }
+
+        const customIdParts = customId.split('.');
+        const number = parseInt(customIdParts[2]) || null;
+        if (number > 10) {
+            return 10;
+        }
+
+        if (number < 1) {
+            return 1;
+        }
     }
 
-    static createCustomId(bannerId, userId) {
+    createCustomId(bannerId, count) {
+        return Summon.createCustomId(bannerId, this.getUser().getUserId(), count);
+    }
+
+    static createCustomId(bannerId, userId, count) {
         if (!userId) {
             throw new Error('Missing user id');
         }
-        return `${this.commandName}.${bannerId || customIdEmpty}.${userId}`;
+        return `${this.commandName}.${bannerId || customIdEmpty}.${count || customIdEmpty}.${userId}`;
     }
 
     static toJSON() {
         return new SlashCommandBuilder()
             .setName(this.commandName)
             .setDescription('Summon a new character')
+            .addNumberOption(option => option.setName('Count')
+                .setDescription('The number of summons to do (max 10)')
+                .setRequired(false))
             .toJSON();
     }
 
